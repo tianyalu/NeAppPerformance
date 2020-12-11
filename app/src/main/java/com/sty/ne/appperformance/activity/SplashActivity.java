316243854197c6e0970bb5611aa110c4ebb4d5a4
tianyi <styzf@qq.com>
2020-12-11 22:27:40 +0800

    黑白屏优化

---
+++ app/src/main/java/com/sty/ne/appperformance/activity/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.sty.ne.appperformance.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.sty.ne.appperformance.MainActivity;
+import com.sty.ne.appperformance.R;
+
+import androidx.annotation.Nullable;
+
+/**
+ * @Author: tian
+ * @UpdateDate: 2020/12/11 10:13 PM
+ */
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        findViewById(R.id.iv_go).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+}
